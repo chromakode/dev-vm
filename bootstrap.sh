f4df2c5a16fb730a766c496e356ed89193d79663
Max Goodman <c@chromakode.com>
2017-09-19 18:12:25 -0700

    Update to new-style overlayfs mount with work dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@
 USER=ubuntu
 
 # ensure it exists and has the correct permissions
-mkdir /mnt/shared-host /mnt/shared-local /mnt/shared
-chown ubuntu:ubuntu /mnt/shared-local /mnt/shared
+mkdir /mnt/shared-host /mnt/shared-local /mnt/shared-work /mnt/shared
+chown ubuntu:ubuntu /mnt/shared-local /mnt/shared-work /mnt/shared
 
 # add 9p to initrd so mountall can mount these filesystems early on during boot
 cat <<END >> /etc/initramfs-tools/modules
@@ -18,7 +18,7 @@ update-initramfs -u
 # set up the fstab entries for the passthrough and overlay filesystems
 cat <<END >> /etc/fstab
 src-passthrough /mnt/shared-host 9p ro,trans=virtio,version=9p2000.L 0 0
-overlayfs /mnt/shared overlayfs lowerdir=/mnt/shared-host,upperdir=/mnt/shared-local 0 0
+overlay /mnt/shared overlay lowerdir=/mnt/shared-host,upperdir=/mnt/shared-local,workdir=/mnt/shared-work 0 0
 END
 
 # mount them right now
